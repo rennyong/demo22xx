ecf179974f9338660dcab3fe468ea9c3221ac74f
renny <rennyong@gmail.com>
2022-12-08 20:36:20 +0800

    Initial

---
+++ src/main/java/com/example/demo22xx/Demo22xxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo22xx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo22xxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo22xxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo22xx/Demo22xxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo22xx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22xxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
